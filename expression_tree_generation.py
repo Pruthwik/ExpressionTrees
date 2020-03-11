dd2fa2faabd024f0083944fdfb6aa4ce018d0747
Pruthwik <pruthwikmishra@gmail.com>
2020-03-11 20:32:51 +0530

    Add files via upload

---
+++ expression_tree_generation.py
@@ -0,0 +1,58 @@
+# The code is similar to a youtube computerphile video
+# by Professor Thorsten Altenkirch
+
+
+class Expr(object):
+    pass
+
+
+class Const(Expr):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Var(Expr):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Plus(Expr):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return '( ' + str(self.left) + ' + ' + str(self.right) + ' )'
+
+    def eval(self, env):
+        evalExpr = ''.join([ch if ch not in env else str(env[ch]) for ch in str(self)])
+        return eval(evalExpr)
+
+
+class Times(Expr):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return '( ' + str(self.left) + ' * ' + str(self.right) + ' )'
+
+    def eval(self, env):
+        evalExpr = ''.join([ch if ch not in env else str(env[ch]) for ch in str(self)])
+        return eval(evalExpr)
+
+
+def main():
+    e1 = Times(Const(3), Var('x'))
+    e2 = Plus(e1, Var('y'))
+    print(e2.eval({'x': 2, 'y': 3}))
+
+
+if __name__ == '__main__':
+    main()
